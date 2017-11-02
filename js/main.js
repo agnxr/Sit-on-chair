59a1f25e3772074f923d00283ab697c01e23fb4d
agnxr <ar21@o2.pl>
2017-11-02 15:55:53 +0100

    add chairs img

--- js/main.js
+++ js/main.js
@@ -1,19 +1,50 @@
-<script>
-var slideIndex = 1;
-showDivs(slideIndex);
+document.addEventListener("DOMContentLoaded", function(){
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+/* slider with chairs - header */
+    var next = document.getElementById('nextPicture');
+    var prev = document.getElementById('prevPicture');
+
+    var array = document.getElementsByClassName('slider');
+
+
+    var index=0;
+
+var visibleElement = array[0].firstElementChild;
+visibleElement.classList.add("visible");
+
+nextPicture.addEventListener("click", event1);
+
+function event1(){
+
+ for (i = 0; i < array[0].children.length; i++) {
+   if(array[0].children[i].classList = "visible"){
+     array[0].children[i].classList.remove('visible')
+   }
+ }
+   array[0].children[index].classList.add('visible')
+     index = index + 1;
+     if(index>=array[0].children.length){
+       index = index - 1;
+     }
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
+prevPicture.addEventListener("click", event2);
+
+function event2(){
+
+for (i = 0; i < array[0].children.length; i++) {
+ if(array[0].children[i].classList = "visible"){
+   array[0].children[i].classList.remove('visible')
+ }
 }
-</script>
+
+ array[0].children[index].classList.add('visible')
+   index = index - 1;
+   if(index<0){
+     index = 0;
+   }
+}
+
+/* end of slider */
+
+})
